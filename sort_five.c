9b84398e21095dcc17f9f6d2ec53cba103a651a0
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2023-07-19 04:44:32 +0200

    sort_five not working yet

---
+++ sort_five.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_five.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 22:40:30 by mflury            #+#    #+#             */
+/*   Updated: 2023/07/18 22:49:28 by mflury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// sort 5 numbers.
+void	sort_five(t_stack *stack, t_data *data)
+{
+	data->i = 0;
+	while (data->i < 2)
+	{
+		if (stack->a[0] > stack->a[1] && stack->a[0] > stack->a[2]
+			&& stack->a[0] > stack->a[3] && stack->a[0] > stack->a[4])
+			{
+				ft_push(stack, data, "pa");
+				
+			}
+		else
+			ft_rotate(stack, data, "ra");
+		data->i++;
+	}
+	sort_three(stack, data);
+	ft_push(stack, data, "pb");
+	ft_push(stack, data, "pb");
+}
