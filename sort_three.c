3ed1a51539108112b9aeb3985691040f97ae083c
FizzY WizzY <102967945+FizzYxWizzY@users.noreply.github.com>
2023-07-19 04:41:29 +0200

    working sort three

---
+++ sort_three.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflury <mflury@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 22:39:24 by mflury            #+#    #+#             */
+/*   Updated: 2023/07/18 22:52:46 by mflury           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// sort 3 numbers.
+void	sort_three(t_stack *stack, t_data *data)
+{
+	while (!is_sorted(stack->a))
+	{
+		if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2]
+			&& stack->a[2] < stack->a[0])
+			ft_rotate(stack, data, "ra");
+		else if (stack->a[0] > stack->a[1] && stack->a[1] < stack->a[2]
+			&& stack->a[0] > stack->a[2])
+			ft_reverse_rotate(stack, data, "rra");
+		else if (stack->a[0] > stack->a[1] && stack->a[0] < stack->a[2]
+			&& stack->a[1] < stack->a[2])
+			ft_swap(stack, data, "sa");
+		else if (stack->a[0] > stack->a[1] && stack->a[1] > stack->a[2]
+			&& stack->a[0] > stack->a[2])
+		{
+			ft_swap(stack, data, "sa");
+			ft_rotate(stack, data, "ra");
+		}
+		else if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2]
+			&& stack->a[1] < stack->a[2])
+		{
+			ft_swap(stack, data, "sa");
+			ft_reverse_rotate(stack, data, "rra");
+		}
+	}
+}
